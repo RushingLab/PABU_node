e300d994ad8e098839edc0ee12485d477f15040f
djklement <dklement127@gmail.com>
2024-01-18 11:14:04 -0500

    Updating raw data.

---
+++ R_scripts/01_beep-data-cleaning.R
@@ -0,0 +1,21 @@
+library(celltracktech)
+library(DBI)
+start <- Sys.time()
+
+####SETTINGS#####
+my_token <- "your token here"
+db_name <- "mydb"
+myproject <- "CTT Project Name" #this is your project name on your CTT account
+conn <- dbConnect(RPostgres::Postgres(), dbname=db_name)
+################
+outpath <- "~/Documents/data/radio_projects/myproject" #where your downloaded files are to go
+get_my_data(my_token, "~/Documents/data/radio_projects/myproject", conn, myproject=myproject)
+update_db(conn, outpath, myproject)
+dbDisconnect(conn)
+
+#findfiles(outpath, "directory path where you want your caught files to go")
+
+time_elapse <- Sys.time() - start
+print(time_elapse)
+
+import_node_data(conn, outpath, myproject="your project name")
